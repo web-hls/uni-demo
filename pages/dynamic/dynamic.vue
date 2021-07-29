1dba693de7ee73785c3cc1fb65772beeece5df68
黄丽珊 <huanglishan@qdama.cn>
2021-07-29 18:14:13 +0800

    pages

---
+++ pages/dynamic/dynamic.vue
@@ -0,0 +1,22 @@
+<template>
+	<view>
+		动态页面
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				
+			}
+		},
+		methods: {
+			
+		}
+	}
+</script>
+
+<style>
+
+</style>
---
+++ pages/my/my.vue
@@ -0,0 +1,60 @@
+<template>
+	<view class="content">
+
+		<text class="my-title">我的页面</text>
+
+		<view class="image">
+			<image class="image" src="../../static/logo.png" mode=""></image>
+		</view>
+
+		<view class="body-container">
+			<text>{{ meaasge }}</text>
+		</view>
+
+		<view class="body-button">
+			<button type="primary" @click="goto('/pages/index/index')">通过方法跳转到首页</button>
+		</view>
+
+	</view>
+</template>
+
+<script>
+	
+	export default {
+		data() {
+			return {
+				meaasge:'开始学习我的uni-app啦'
+			}
+		},
+		methods: {
+			 goto(url) {
+				uni.switchTab({
+					url:url
+			  })
+			}
+		}
+	}
+</script>
+
+<style lang="scss" scoped>
+	.content {
+		padding: 20upx;
+		.my-title {
+			color: #2B9939;
+			font-weight: bold;
+		}
+		.image {
+			padding: 10upx;
+			border-radius: 250upx;
+			width: $uni-img-size-lg;
+			height: $uni-img-size-lg;
+		}
+		.body-container {
+			padding-top: 20upx;
+		}
+		.body-button {
+			padding-top: 40upx;
+		}
+	}
+
+</style>
