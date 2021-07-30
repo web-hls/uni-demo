9d8e06921e392afa83f47a8e4088ce5925f1bf78
黄丽珊 <huanglishan@qdama.cn>
2021-07-30 18:28:05 +0800

    add-register

---
+++ pages/register/register.vue
@@ -0,0 +1,38 @@
+<template>
+	<view class="register-content">
+		<image class="cube-logo" src="https://labfile.oss.aliyuncs.com/courses/4715/logoNew.png" mode=""></image>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				
+			}
+		},
+		methods: {
+			
+		}
+	}
+</script>
+
+<style lang="scss" scoped>
+	.register-content {
+	  background: #f7e8d5;
+	  position: fixed;
+	  overflow-y: scroll;
+	  top: 0;
+	  left: 0;
+	  right: 0;
+	  bottom: 0;
+		>image {
+			width: 100%;
+			height: 60%;
+			.cube-logo {
+				width: 100%;
+			}
+		}
+	}
+
+</style>
