4882298335fda77050b7b08e29ba518af97e6d15
黄丽珊 <huanglishan@qdama.cn>
2021-08-02 16:43:50 +0800

    login-and-register-ui

--- pages/login/login.vue
+++ pages/login/login.vue
@@ -1,6 +1,18 @@
 <template>
-	<view>
-		<text>登录页面</text>
+	<view class="login-content">
+		<image class="cube-logo" src="https://labfile.oss.aliyuncs.com/courses/4715/1627885535(1).png"></image>
+		<view class="login-input">
+			<input class="uni-input input" focus placeholder="请输入账号" placeholder-style="color:#cccccc"/>
+			<input class="uni-input input" focus placeholder="请输入密码" password placeholder-style="color:#cccccc"/>
+		</view>
+		<view class="login-button">
+			<button class="button" type="default">登录</button>
+		</view>
+		
+		<view class="relevant-description">
+			<view class="description-one">还未注册，<text class="color-mark" @click="goToRegister">点击注册</text></view>
+			<view class="description-two">点击注册即代表您同意<text class="color-mark">《用户协议》</text>和<text class="color-mark">《隐私协议》</text></view>
+		</view>
 	</view>
 </template>
 
@@ -12,11 +24,75 @@
 			}
 		},
 		methods: {
-			
+			goToRegister(){
+				uni.navigateTo({
+				    url: '/pages/register/register'
+				});
+			}
 		}
 	}
 </script>
 
-<style>
+<style lang="scss" scoped>
+	.login-content {
+	  background: #f7e8d5;
+	  position: fixed;
+	  overflow-y: scroll;
+	  top: 0;
+	  left: 0;
+	  right: 0;
+	  bottom: 0;
+		>image {
+			margin-top: 100rpx;
+			width: 100%;
+		}
+		.login-input {
+			.input {
+				border: 1rpx solid #cccccc;
+				border-radius: 25rpx;
+				background: $uni-bg-color;
+				padding: 28rpx;
+				width: 80%;
+				text-align: left;
+				display: flex;
+				align-items: center;
+				justify-content: center;
+				margin: 20rpx auto;
+				height: 55rpx;
+			}
+		}
+		.login-button {
+			margin: 20rpx auto;
+			width: 85%;
+			background: #E6A23C;
+			border-radius: 25rpx;
+			.button {
+				color: #FFFFFF;
+				background: #E6A23C;
+				margin-top: 40rpx;
+				
+			}
+		}
+		.relevant-description {
+			text-align: center;
+			.description-one {
+				font-size: 36rpx;
+				font-weight: 700;
+				color:black;
+				margin-top: 100rpx;
+				margin-bottom: 20rpx;
+			}
+			.description-two {
+				font-size: 28rpx;
+				font-weight: 400;
+				color: gray;
+				margin-bottom: 100rpx;
+			}
+			.color-mark {
+				color:#E6A23C
+			}
+		}
+		
+	}
 
 </style>
--- pages/register/register.vue
+++ pages/register/register.vue
@@ -1,6 +1,19 @@
 <template>
 	<view class="register-content">
-		<image class="cube-logo" src="https://labfile.oss.aliyuncs.com/courses/4715/logoNew.png" mode=""></image>
+		<image class="cube-logo" src="https://labfile.oss.aliyuncs.com/courses/4715/1627885535(1).png"></image>
+		<view class="register-input">
+			<input class="uni-input input" focus placeholder="请输入账号" placeholder-style="color:#cccccc"/>
+			<input class="uni-input input" focus placeholder="请输入密码" password placeholder-style="color:#cccccc"/>
+			<input class="uni-input input" focus placeholder="请确认密码" password placeholder-style="color:#cccccc"/>
+		</view>
+		<view class="register-button">
+			<button class="button" type="default">注册</button>
+		</view>
+		
+		<view class="relevant-description">
+			<view class="description-one">已有账号，<text class="color-mark" @click="goToLogin">立即登录</text></view>
+			<view class="description-two">点击注册即代表您同意<text class="color-mark">《用户协议》</text>和<text class="color-mark">《隐私协议》</text></view>
+		</view>
 	</view>
 </template>
 
@@ -12,7 +25,11 @@
 			}
 		},
 		methods: {
-			
+			goToLogin(){
+				uni.navigateTo({
+				    url: '/pages/login/login'
+				});
+			}
 		}
 	}
 </script>
@@ -27,12 +44,56 @@
 	  right: 0;
 	  bottom: 0;
 		>image {
+			margin-top: 100rpx;
 			width: 100%;
-			height: 60%;
-			.cube-logo {
-				width: 100%;
+		}
+		.register-input {
+			.input {
+				border: 1rpx solid #cccccc;
+				border-radius: 25rpx;
+				background: $uni-bg-color;
+				padding: 28rpx;
+				width: 80%;
+				text-align: left;
+				display: flex;
+				align-items: center;
+				justify-content: center;
+				margin: 20rpx auto;
+				height: 55rpx;
 			}
 		}
+		.register-button {
+			margin: 20rpx auto;
+			width: 85%;
+			background: #E6A23C;
+			border-radius: 25rpx;
+			.button {
+				color: #FFFFFF;
+				background: #E6A23C;
+				margin-top: 40rpx;
+				
+			}
+		}
+		.relevant-description {
+			text-align: center;
+			.description-one {
+				font-size: 36rpx;
+				font-weight: 700;
+				color:black;
+				margin-top: 100rpx;
+				margin-bottom: 20rpx;
+			}
+			.description-two {
+				font-size: 28rpx;
+				font-weight: 400;
+				color: gray;
+				margin-bottom: 100rpx;
+			}
+			.color-mark {
+				color:#E6A23C
+			}
+		}
+		
 	}
 
 </style>
